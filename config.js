05a963590d269a31bc48e47de072870c70e76823
tran <fidtran@gmail.com>
2021-02-18 14:59:36 -0500

    update env and config

--- config.js
+++ config.js
@@ -10,4 +10,5 @@ module.exports = {
 		process.env.NODE_ENV == "development"
 			? process.env.REACT_APP_API_BASE_URL
 			: "https://lamodebackend2.herokuapp.com/",
+	cloudinaryUrl: process.env.REACT_APP_CLOUDINARY_URL,
 };
